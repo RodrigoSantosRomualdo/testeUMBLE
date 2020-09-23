63faa7aab22cefa13355caad9fb48552b45cf4d8
Rodrigo Romualdo <rodrigo.romualdo@intelliway.com.br>
2020-09-22 21:44:54 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+
+
+app.get("/", (req, res) => {
+  res.send("<h1>CHEGOU AQUI</h1>")
+})
+
+
+
+app.listen(3000, function() {
+  console.log("Server Rodando na URL http://localhost:3000");
+});
